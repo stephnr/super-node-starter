4f21d0996dea60944586f351a9fc684b51297d65
Stephen Rodriguez <steprodriguez10@gmail.com>
2016-08-25 12:24:33 -0400

    testing for route existence

---
+++ __tests__/endpoints.spec.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/*===============================
+=            MODULES            =
+===============================*/
+
+const should = require('chai').should();
+const app = require('./app.setup').default;
+
+/*=====  End of MODULES  ======*/
+
+const routeRegex = app._router.stack.filter(r => r.name === 'router').map(r => String(r.regexp));
+
+describe('routes', () => {
+
+  it('should have auth routes', () => {
+    routeRegex.indexOf('/^\\/auth\\/?(?=\\/|$)/i').should.not.equal(-1);
+  });
+
+  it('should have user routes', () => {
+    routeRegex.indexOf('/^\\/users\\/?(?=\\/|$)/i').should.not.equal(-1);
+  });
+
+});
+
